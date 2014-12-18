977f66859a4e315c6b10263e66a92f640a9bd966
Dave Boling <kadowki@gmail.com>
2014-12-17 22:52:42 -0600

    Added final (hopefully) schema changes and test data entry

--- db/tables/segments.sql
+++ db/tables/segments.sql
@@ -1,6 +1,7 @@
 create table segments(
   id serial primary key,
   creator integer not null references users(id),
+  body text not null,
   stream_id integer not null references streams(id),
   sort_order integer null
 );
--- scripts/data.sql
+++ scripts/data.sql
@@ -12,23 +12,46 @@ insert into users (id, username, password) values (2, 'sue', '$2a$08$Ekv2PGTlRzM
 --project inserts
 insert into projects (project_name, owner_id) values ('Bob Project', 1);
 delete from projects; --offset sequence
+
 insert into projects (id, project_name, owner_id) values (1, 'Bob Project', 1);
 insert into projects (project_name, owner_id) values ('Bob Other Project', 1);
 insert into projects (project_name, owner_id) values ('Sue Project', 2);
 
-  
---collaborators inserts
+
+--collaborators inserts (adds Sue as a collaborator on Bob Project)
+insert into collaborators (project_id, user_id) values (1, 2);
 
 --stream inserts
 insert into streams (project_id, name) values (1, 'null');
+insert into streams (project_id, name) values (1, 'null');
 delete from streams; --offset for sequence
+
 insert into streams (id, project_id, name) values (1, 1, 'Stream One');
-insert into streams (project_id, name) values (1, 'Stream Two');
+insert into streams (id, project_id, name) values (2, 1, 'Stream Two');
 insert into streams (project_id, name) values (1, 'Stream Three');
 
 --segment inserts
+insert into segments (creator, stream_id, body) values (2, 1, '');
+insert into segments (creator, stream_id, body) values (2, 1, '');
+delete from segments; --offse for sequence
 
+insert into segments (id, creator, stream_id, body) values (1, 2, 1, 'Added my PDF file to this stream for everyone to read');
+insert into segments (id, creator, stream_id, body) values (2, 1, 2, 'It is coming along.');
 
+--replys inserts
+insert into replys (creator, segment_id, body) values (1, 1, 'Thank you Sue, I will read it very soon.');
+insert into replys (creator, segment_id, body) values (2, 2, 'Great to hear Bob!');
 
 
 --attachment inserts
+insert into attachments (url, segment_id) values ('http://somewhere.com/something.pdf', 1);
+insert into attachments (url, segment_id) values ('http://somewhere.com/something2.pdf', 1);
+
+
+
+
+
+
+
+
+
