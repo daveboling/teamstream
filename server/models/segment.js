575302894f1477f1a5365206e7992ada2a6061b6
Dave Boling <kadowki@gmail.com>
2014-12-21 12:49:37 -0600

    Added attachments to Segment.getAll and associated test

--- server/models/segment.js
+++ server/models/segment.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var pg = require('../config/postgres/manager');
+var pg     = require('../config/postgres/manager'),
+    async  = require('async'),
+    Reply = require('./Reply');
 
 function Segment(){
 }
@@ -11,8 +13,22 @@ Segment.create = function(obj, user, cb){
 
 //needs to be modified where it also gets attachments
 Segment.getAll = function(obj, cb){
-  pg.query('select * from get_segments($1)', [obj.sid], cb);
+  pg.query('select * from get_segments($1)', [obj.sid], function(err, results){
+    if(err){return cb(err);}
+    async.map(results.rows, iterator, function(err, segments){
+      if(err){return cb(err);}
+      cb(null, segments);
+    });
+  });
 };
 
-
 module.exports = Segment;
+
+function iterator(row, cb){
+  Reply.populate(row.id, function(err, replys){
+    row.replys = replys.rows;
+    cb(err, row);
+  });
+}
+
+
