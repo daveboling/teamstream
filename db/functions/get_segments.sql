2d36bf6742cc2ed02ed06d306e3fcc3a99979ab8
Dave Boling <kadowki@gmail.com>
2014-12-21 12:22:42 -0600

    Fixed get_segments.sql to also get attachments

--- db/functions/get_segments.sql
+++ db/functions/get_segments.sql
@@ -1,12 +1,14 @@
 CREATE or REPLACE FUNCTION get_segments(sid integer)
-RETURNS TABLE(id integer, created_on timestamp, segment_creator integer, body text, creator varchar) AS $$
+RETURNS TABLE(id integer, created_on timestamp, segment_creator integer, body text, creator varchar, attachments varchar[]) AS $$
 BEGIN
 
 RETURN QUERY
-SELECT segments.id, segments.date_created, segments.creator,  segments.body, users.username
+SELECT segments.id, segments.date_created, segments.creator,  segments.body, users.username, array_agg(DISTINCT attachments.url)
 FROM segments
 INNER JOIN users on segments.creator = users.id
-WHERE segments.stream_id = sid;
+LEFT OUTER JOIN attachments on attachments.segment_id = segments.id
+WHERE segments.stream_id = sid
+GROUP BY segments.id, segments.date_created, segments.creator,  segments.body, users.username;
 
 END;
 $$ LANGUAGE plpgsql;
