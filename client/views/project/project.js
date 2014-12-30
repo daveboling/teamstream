0d4171f20afc7ae3b9330782094a6987cbfea7eb
Dave Boling <kadowki@gmail.com>
2014-12-30 16:31:57 -0600

    Added HTML and CSS for toggleable control bar.

--- client/views/project/project.js
+++ client/views/project/project.js
@@ -5,10 +5,30 @@
   project.controller('ProjectCtrl', ['$scope', '$stateParams', 'Project', function($scope, $stateParams, Project){
     $scope.projectId = $stateParams.pid;
     $scope.project = {};
+    $scope.barIsVisible = true;
+    $scope.activity = [
+      'Dave hit the jackpot',
+      'Joe made a new note.',
+      'Epsilon meowed because she wanted out.'
+    ];
 
     Project.findOne($scope.projectId).then(function(res){
       $scope.project = res.data;
     });
+
+    //sidebar toggle
+    $scope.toggleSidebar = function(){
+      if($scope.barIsVisible){
+        $('#side-bar').animate({right: '-300px'});
+        $('#toggle').animate({right: '-5px'});
+      }
+      else{
+        $('#side-bar').animate({right: '2px'});
+        $('#toggle').animate({right: '295px'});
+      }
+      //toggle
+      $scope.barIsVisible = !$scope.barIsVisible;
+    };
 
   }]);
 })();
