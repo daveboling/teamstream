c3a637a7d85508e94156438ee6fd4acbaffe34cd
Dave Boling <kadowki@gmail.com>
2014-12-31 21:54:23 -0600

    Added users.email to get_segments and get_replies SQL files. Added gravatar to segments.

--- db/functions/get_replys.sql
+++ db/functions/get_replys.sql
@@ -1,9 +1,9 @@
 CREATE or REPLACE FUNCTION get_replys(segId integer)
-RETURNS TABLE(id integer, body text, created_on timestamp, segment_id integer, creator varchar) AS $$
+RETURNS TABLE(id integer, body text, created_on timestamp, segment_id integer, creator varchar, email varchar) AS $$
 BEGIN
 
 RETURN QUERY
-SELECT replys.id, replys.body, replys.date_created, replys.segment_id, users.username
+SELECT replys.id, replys.body, replys.date_created, replys.segment_id, users.username, users.email
 FROM replys
 INNER JOIN users on replys.creator = users.id
 WHERE replys.segment_id = segId;
--- db/functions/get_segments.sql
+++ db/functions/get_segments.sql
@@ -1,14 +1,14 @@
 CREATE or REPLACE FUNCTION get_segments(sid integer)
-RETURNS TABLE(id integer, created_on timestamp, segment_creator integer, body text, creator varchar, attachments varchar[], edited_by varchar, date_edited timestamp) AS $$
+RETURNS TABLE(id integer, created_on timestamp, segment_creator integer, body text, creator varchar, email varchar,attachments varchar[], edited_by varchar, date_edited timestamp) AS $$
 BEGIN
 
 RETURN QUERY
-SELECT segments.id, segments.date_created, segments.creator,  segments.body, users.username, array_agg(DISTINCT attachments.url), segments.edited_by, segments.date_edited
+SELECT segments.id, segments.date_created, segments.creator,  segments.body, users.username, users.email, array_agg(DISTINCT attachments.url), segments.edited_by, segments.date_edited
 FROM segments
 INNER JOIN users on segments.creator = users.id
 LEFT OUTER JOIN attachments on attachments.segment_id = segments.id
 WHERE segments.stream_id = sid
-GROUP BY segments.id, segments.date_created, segments.creator,  segments.body, users.username, segments.edited_by, segments.date_edited;
+GROUP BY segments.id, segments.date_created, segments.creator,  segments.body, users.username, segments.edited_by, segments.date_edited, users.email;
 
 END;
 $$ LANGUAGE plpgsql;
