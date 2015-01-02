4b88066e7cc56f3b113bd2a299f298ce99ffa8a7
Dave Boling <kadowki@gmail.com>
2015-01-02 01:44:12 -0600

    Added Gumby navbar with some modifications.

--- client/views/nav/nav.js
+++ client/views/nav/nav.js
@@ -6,6 +6,7 @@
       $scope.logout = function(){
         window.dispatchEvent(new CustomEvent('logout', {}));
         User.logout().then(function(){
+          console.log('clicked');
           $rootScope.rootuser = null;
           $state.go('home');
         });
