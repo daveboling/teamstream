10d71d8cf3c9c887c54c28fbe4b70fe9e5dfe7a8
Dave Boling <kadowki@gmail.com>
2015-01-04 01:56:05 -0600

    UI related fix to avatar where when logging out, the old menu was not hidden immediately

--- client/views/nav/nav.js
+++ client/views/nav/nav.js
@@ -5,6 +5,7 @@
     .controller('NavCtrl', ['$rootScope', '$scope', '$state', 'User', function($rootScope, $scope, $state, User){
       $scope.logout = function(){
         window.dispatchEvent(new CustomEvent('logout', {}));
+        $('.change').hide().fadeIn(1000);
         User.logout().then(function(){
           $rootScope.rootuser = null;
           $state.go('home');
